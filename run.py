da724780be7e8f120670da9f19e1a11c6ce51b4e
zj9495 <zj9495@gmail.com>
2021-07-23 13:40:06 +0800

    feat: 支持hda hdu站点

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 import sys
-
+from NexusPHP.tasks.say_thanks import main as saythanks
+from NexusPHP.tasks.sign_in import main as signin
+from NexusPHP.tasks.hda_sign_in import main as hdasignin
+from NexusPHP.tasks.hdu_sign_in import main as hdusignin
 
 if __name__ == '__main__':
     project = sys.argv[1]
     action = sys.argv[2]
 
     if project == 'nexusphp':
-        from NexusPHP.tasks.say_thanks import main as saythanks
-        from NexusPHP.tasks.sign_in import main as signin
         eval('%s()' % action)
+    elif project == 'hda':
+        hdasignin()
+    elif project == 'hdu':
+        hdusignin()
     else:
         print('命令参数1 不正确!')
